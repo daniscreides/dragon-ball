fc1ad22f40b000baf39e886f350fb5db0bea973c
Dani <danielisouza436@gmail.com>
2025-12-20 19:02:26 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from "react-router-dom";
 import { useState } from "react";
-
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Sobre from "./pages/Sobre";
